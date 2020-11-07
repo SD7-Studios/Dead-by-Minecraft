d32f0d99e58bfee2ea7ca92a1bcad4b50dcac906
Ryan Jones <mail2ryanjones@gmail.com>
2020-11-07 13:09:58 -0500

    Updated gamerules in lobby and game worlds

--- src/main/kotlin/org/wordandahalf/spigot/deadbyminecraft/worlds/GameWorld.kt
+++ src/main/kotlin/org/wordandahalf/spigot/deadbyminecraft/worlds/GameWorld.kt
@@ -2,6 +2,7 @@ package org.wordandahalf.spigot.deadbyminecraft.worlds
 
 import com.grinderwolf.swm.api.world.SlimeWorld
 import org.bukkit.Bukkit
+import org.bukkit.GameRule
 import org.bukkit.World
 import org.wordandahalf.spigot.deadbyminecraft.config.GameWorldConfig
 import org.wordandahalf.spigot.deadbyminecraft.game.behaviors.Behavior
@@ -29,6 +30,20 @@ data class GameWorld(val world: SlimeWorld, val config: GameWorldConfig) : Dispo
             behavior = gameBehavior
 
         // Initialize the world based on the provided configuration
+
+        bukkit.setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false)
+        bukkit.setGameRule(GameRule.DISABLE_RAIDS, true)
+        bukkit.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)
+        bukkit.setGameRule(GameRule.DO_FIRE_TICK, false)
+        bukkit.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, true)
+        bukkit.setGameRule(GameRule.DO_MOB_SPAWNING, false)
+        bukkit.setGameRule(GameRule.DO_PATROL_SPAWNING, false)
+        bukkit.setGameRule(GameRule.DO_TRADER_SPAWNING, false)
+        bukkit.setGameRule(GameRule.KEEP_INVENTORY, true)
+        bukkit.setGameRule(GameRule.NATURAL_REGENERATION, false)
+        bukkit.setGameRule(GameRule.REDUCED_DEBUG_INFO, true)
+        bukkit.setGameRule(GameRule.SHOW_DEATH_MESSAGES, false)
+
         bukkit.time = config.time
     }
 
--- src/main/kotlin/org/wordandahalf/spigot/deadbyminecraft/worlds/LobbyWorld.kt
+++ src/main/kotlin/org/wordandahalf/spigot/deadbyminecraft/worlds/LobbyWorld.kt
@@ -2,6 +2,7 @@ package org.wordandahalf.spigot.deadbyminecraft.worlds
 
 import com.grinderwolf.swm.api.world.SlimeWorld
 import org.bukkit.Bukkit
+import org.bukkit.GameRule
 import org.bukkit.World
 import org.wordandahalf.spigot.deadbyminecraft.config.LobbyWorldConfig
 import org.wordandahalf.spigot.deadbyminecraft.game.behaviors.Behavior
@@ -30,6 +31,20 @@ data class LobbyWorld(val world: SlimeWorld, val config: LobbyWorldConfig) : Dis
             behavior = lobbyBehavior
 
         // Initialize the world based on the provided configuration
+
+        bukkit.setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false)
+        bukkit.setGameRule(GameRule.DISABLE_RAIDS, true)
+        bukkit.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)
+        bukkit.setGameRule(GameRule.DO_FIRE_TICK, false)
+        bukkit.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, true)
+        bukkit.setGameRule(GameRule.DO_MOB_SPAWNING, false)
+        bukkit.setGameRule(GameRule.DO_PATROL_SPAWNING, false)
+        bukkit.setGameRule(GameRule.DO_TRADER_SPAWNING, false)
+        bukkit.setGameRule(GameRule.KEEP_INVENTORY, true)
+        bukkit.setGameRule(GameRule.NATURAL_REGENERATION, false)
+        bukkit.setGameRule(GameRule.REDUCED_DEBUG_INFO, true)
+        bukkit.setGameRule(GameRule.SHOW_DEATH_MESSAGES, false)
+
         bukkit.time = config.time
     }
 
